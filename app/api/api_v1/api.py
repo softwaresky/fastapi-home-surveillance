85fa1defad0e2e146b51d358c0685968eaffdb55
softwaresky <mihail.sofeski@gmail.com>
2022-02-11 10:00:13 +0100

    hotfix

--- app/api/api_v1/api.py
+++ app/api/api_v1/api.py
@@ -1,7 +1,4 @@
 from fastapi import APIRouter
-import time
-from app.engine.thread_controller import controller
-
 from app.api.api_v1.endpoints import stream, archive, servo
 
 api_router = APIRouter()
@@ -10,16 +7,3 @@ api_router.include_router(servo.router, prefix="/servo", tags=["servo"])
 api_router.include_router(stream.router, prefix="/stream", tags=["stream"])
 api_router.include_router(archive.router, prefix="/archive", tags=["archive"])
 
-@api_router.on_event("startup")
-def startup_router():
-    time.sleep(1)
-    controller.start()
-    pass
-
-
-@api_router.on_event("shutdown")
-def shutdown_router():
-    controller.stop_children_threads()    # all threads can't be stopped
-    controller.is_running = False
-    controller.join()
-    pass
--- app/api/api_v1/endpoints/servo.py
+++ app/api/api_v1/endpoints/servo.py
@@ -1,4 +1,5 @@
 import pprint
+import time
 from typing import Any, Optional
 from fastapi import APIRouter, WebSocket
 from app.engine.thread_controller import controller
@@ -6,6 +7,20 @@ from app.schemas.servo import ServoUpdate
 
 
 router = APIRouter()
+
+@router.on_event("startup")
+def startup_router():
+    time.sleep(1)
+    controller.start()
+    pass
+
+
+@router.on_event("shutdown")
+def shutdown_router():
+    controller.stop_children_threads()    # all threads can't be stopped
+    controller.is_running = False
+    controller.join()
+    pass
 
 @router.put("/move/")
 async def move_by_axis(
