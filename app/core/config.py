877885f789e1c394d2a10aedd43be503011c9b9d
softwaresky <mihail.sofeski@gmail.com>
2022-02-11 15:43:15 +0100

    change pins from normal to pmw

--- app/core/config.py
+++ app/core/config.py
@@ -18,9 +18,9 @@ class Settings(BaseSettings):
 
     SERVO_PIN_MAP: Optional[dict] = {
         # "pan_pin": 11, # BOARD
-        "pan_pin": 17, # BCM
+        "pan_pin": 13, # BCM
         # "tilt_pin": 18 # BOARD
-        "tilt_pin": 24 # BCM
+        "tilt_pin": 12 # BCM
     }
 
     DHT_PIN = 4
